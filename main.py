f0b1979d7ea991de3dc39f80d1b3fe442dca0f2f
pointoflight <f20180209@goa.bits-pilani.ac.in>
2021-06-01 02:04:43 +0530

    update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 import warnings
 from random import sample
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
@@ -23,9 +22,9 @@ parser = argparse.ArgumentParser(description='Crystal Graph Convolutional Neural
 parser.add_argument('data_options', metavar='OPTIONS', nargs='+',
                     help='dataset options, started with the path to root dir, '
                          'then other options')
-parser.add_argument('--task', choices=['regression', 'classification'],
-                    default='regression', help='complete a regression or '
-                                                   'classification task (default: regression)')
+parser.add_argument('--task', choices=['regression', 'classification', "r", "c"], nargs="+",
+                    default=['r'], help='complete a regression or '
+                                                 'classification task (default: regression)')
 parser.add_argument('--disable-cuda', action='store_true',
                     help='Disable CUDA')
 parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
@@ -52,19 +51,19 @@ parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 train_group = parser.add_mutually_exclusive_group()
 train_group.add_argument('--train-ratio', default=None, type=float, metavar='N',
-                    help='number of training data to be loaded (default none)')
+                         help='number of training data to be loaded (default none)')
 train_group.add_argument('--train-size', default=None, type=int, metavar='N',
                          help='number of training data to be loaded (default none)')
 valid_group = parser.add_mutually_exclusive_group()
 valid_group.add_argument('--val-ratio', default=0.1, type=float, metavar='N',
-                    help='percentage of validation data to be loaded (default '
-                         '0.1)')
+                         help='percentage of validation data to be loaded (default '
+                              '0.1)')
 valid_group.add_argument('--val-size', default=None, type=int, metavar='N',
                          help='number of validation data to be loaded (default '
                               '1000)')
 test_group = parser.add_mutually_exclusive_group()
 test_group.add_argument('--test-ratio', default=0.1, type=float, metavar='N',
-                    help='percentage of test data to be loaded (default 0.1)')
+                        help='percentage of test data to be loaded (default 0.1)')
 test_group.add_argument('--test-size', default=None, type=int, metavar='N',
                         help='number of test data to be loaded (default 1000)')
 
@@ -76,14 +75,16 @@ parser.add_argument('--h-fea-len', default=128, type=int, metavar='N',
                     help='number of hidden features after pooling')
 parser.add_argument('--n-conv', default=3, type=int, metavar='N',
                     help='number of conv layers')
-parser.add_argument('--n-h', default=1, type=int, metavar='N',
+parser.add_argument('--n-h', default=2, type=int, metavar='N',
                     help='number of hidden layers after pooling')
-
+parser.add_argument('--ignore', '-i', nargs='+', metavar='N', type=int,
+                    help='indices of columns to ignore errors')
+parser.add_argument('--freeze', '-f', metavar='N', type=int, help='number of frozen layers')
 args = parser.parse_args(sys.argv[1:])
 
 args.cuda = not args.disable_cuda and torch.cuda.is_available()
 
-if args.task == 'regression':
+if args.task and {'regression', 'r'}:
     best_mae_error = 1e10
 else:
     best_mae_error = 0.
@@ -93,6 +94,7 @@ def main():
     global args, best_mae_error
 
     # load data
+    print("Loading data")  ##
     dataset = CIFData(*args.data_options)
     collate_fn = collate_pool
     train_loader, val_loader, test_loader = get_train_val_test_loader(
@@ -110,6 +112,7 @@ def main():
         return_test=True)
 
     # obtain target value normalizer
+    print("Priming normalizer")  ##
     if args.task == 'classification':
         normalizer = Normalizer(torch.zeros(2))
         normalizer.load_state_dict({'mean': 0., 'std': 1.})
@@ -125,6 +128,7 @@ def main():
         normalizer = Normalizer(sample_target)
 
     # build model
+    print("Training", sample_target.shape[1], "properties")
     structures, _, _ = dataset[0]
     orig_atom_fea_len = structures[0].shape[-1]
     nbr_fea_len = structures[1].shape[-1]
@@ -134,15 +138,28 @@ def main():
                                 h_fea_len=args.h_fea_len,
                                 n_h=args.n_h,
                                 classification=True if args.task ==
-                                                       'classification' else False)
+                                                       'classification' else False,
+                                n_t=sample_target.shape[1])  ##
     if args.cuda:
         model.cuda()
+    # freeze layers
+    if args.freeze:
+        nfreeze = min(args.n_h - 1, args.freeze)
+        for i in range(nfreeze):
+            for param in model.fcs[args.n_h - 2 - i].parameters():
+                param.requires_grad = False  # no prop
+            # for param in model.softpluses[args.n_h - 2 - i].parameters():
+            #     param.requires_grad = False
 
     # define loss func and optimizer
-    if args.task == 'classification':
-        criterion = nn.NLLLoss()
-    else:
-        criterion = nn.MSELoss()
+    criterion = []
+    print("tasks:", ",".join(args.task))
+    for task in args.task:
+        if task in {'dlassification', 'd'}:#broken
+            criterion.append(nn.NLLLoss())
+        else:
+            criterion.append(nn.MSELoss())
+    print(criterion)
     if args.optim == 'SGD':
         optimizer = optim.SGD(model.parameters(), args.lr,
                               momentum=args.momentum,
@@ -152,6 +169,9 @@ def main():
                                weight_decay=args.weight_decay)
     else:
         raise NameError('Only SGD or Adam is allowed as --optim')
+
+    # clear loss files
+    # with open("loss_train.txt", w) as foo
 
     # optionally resume from a checkpoint
     if args.resume:
@@ -171,17 +191,12 @@ def main():
     scheduler = MultiStepLR(optimizer, milestones=args.lr_milestones,
                             gamma=0.1)
 
-    mae_errors = []
-    epochs = []
-
     for epoch in range(args.start_epoch, args.epochs):
         # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch, normalizer)
+        train(train_loader, model, criterion, optimizer, epoch, normalizer, col_ign=args.ignore)
 
         # evaluate on validation set
-        mae_error = validate(val_loader, model, criterion, normalizer)
-        mae_errors.append(mae_error)
-        epochs.append(epoch)
+        mae_error = validate(val_loader, model, criterion, normalizer, epoch, col_ign=args.ignore)
 
         if mae_error != mae_error:
             print('Exit due to NaN')
@@ -190,7 +205,7 @@ def main():
         scheduler.step()
 
         # remember the best mae_eror and save checkpoint
-        if args.task == 'regression':
+        if args.task and {'regression', 'r'}:
             is_best = mae_error < best_mae_error
             best_mae_error = min(mae_error, best_mae_error)
         else:
@@ -202,29 +217,47 @@ def main():
             'best_mae_error': best_mae_error,
             'optimizer': optimizer.state_dict(),
             'normalizer': normalizer.state_dict(),
-            'args': vars(args)
+            'args': {**vars(args), **{"n_t": sample_target.shape[1]}}
         }, is_best)
 
-
-    plt.plot(epochs, mae_errors)
-    plt.xlabel("epochs")
-    plt.ylabel("validation mae")
-    plt.show()
-    # plt.plot([2,3,5,5], [3,5,6,6])
     # test best model
     print('---------Evaluate Model on Test Set---------------')
     best_checkpoint = torch.load('model_best.pth.tar')
     model.load_state_dict(best_checkpoint['state_dict'])
-    validate(test_loader, model, criterion, normalizer, test=True)
+    validate(test_loader, model, criterion, normalizer, epoch, test=True, col_ign=args.ignore)
+    #print(model)  # print model
 
-    # print(torch.cuda.is_available())
+
+def lossfn(outs, targets, ignore, criterion, runtyp, cycle):
+    col_losses = [0.] * outs.shape[1]
+    cols = np.array(range(outs.shape[1]))
+    sum_cols = 0.
+    for i in cols:
+        col_losses[i] = criterion[i % len(criterion)](outs[:, i], targets[:, i])
+    if runtyp == "validation":
+        ofname = "losses_val.txt"
+    elif runtyp == "training":
+        ofname = "losses_train.txt"
+    elif runtyp == "testing":
+        ofname = "losses_test.txt"
+    else:
+        ofname = "running_losses.txt"
+    with open(ofname, "a") as lossfile:
+        for i in col_losses:
+            lossfile.write(str(cycle) + "," + str(i.item()))
+        lossfile.write("\n")
+    if ignore:
+        cols = np.delete(cols, ignore)
+    for i in cols:
+        sum_cols += col_losses[i]
+    return sum_cols
 
 
-def train(train_loader, model, criterion, optimizer, epoch, normalizer):
+def train(train_loader, model, criterion, optimizer, epoch, normalizer, col_ign=[]):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
-    if args.task == 'regression':
+    if args.task and {'regression', 'r'}:
         mae_errors = AverageMeter()
     else:
         accuracies = AverageMeter()
@@ -252,7 +285,7 @@ def train(train_loader, model, criterion, optimizer, epoch, normalizer):
                          input[2],
                          input[3])
         # normalize target
-        if args.task == 'regression':
+        if args.task and {'regression', 'r'}:
             target_normed = normalizer.norm(target)
         else:
             target_normed = target.view(-1).long()
@@ -263,10 +296,10 @@ def train(train_loader, model, criterion, optimizer, epoch, normalizer):
 
         # compute output
         output = model(*input_var)
-        loss = criterion(output, target_var)
+        loss = lossfn(output, target_var, col_ign, criterion, "training", epoch)
 
         # measure accuracy and record loss
-        if args.task == 'regression':
+        if args.task and {'regression', 'r'}:
             mae_error = mae(normalizer.denorm(output.data.cpu()), target)
             losses.update(loss.data.cpu(), target.size(0))
             mae_errors.update(mae_error, target.size(0))
@@ -290,7 +323,7 @@ def train(train_loader, model, criterion, optimizer, epoch, normalizer):
         end = time.time()
 
         if i % args.print_freq == 0:
-            if args.task == 'regression':
+            if args.task and {'regression', 'r'}:
                 print('Epoch: [{0}][{1}/{2}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
@@ -316,10 +349,10 @@ def train(train_loader, model, criterion, optimizer, epoch, normalizer):
                 )
 
 
-def validate(val_loader, model, criterion, normalizer, test=False):
+def validate(val_loader, model, criterion, normalizer, epoch, test=False, col_ign=[]):
     batch_time = AverageMeter()
     losses = AverageMeter()
-    if args.task == 'regression':
+    if args.task and {'regression', 'r'}:
         mae_errors = AverageMeter()
     else:
         accuracies = AverageMeter()
@@ -328,8 +361,8 @@ def validate(val_loader, model, criterion, normalizer, test=False):
         fscores = AverageMeter()
         auc_scores = AverageMeter()
     if test:
-        test_targets = []
-        test_preds = []
+        test_targets = torch.tensor([])
+        test_preds = torch.tensor([])
         test_cif_ids = []
 
     # switch to evaluate mode
@@ -349,7 +382,7 @@ def validate(val_loader, model, criterion, normalizer, test=False):
                              Variable(input[1]),
                              input[2],
                              input[3])
-        if args.task == 'regression':
+        if args.task and {'regression', 'r'}:
             target_normed = normalizer.norm(target)
         else:
             target_normed = target.view(-1).long()
@@ -362,18 +395,18 @@ def validate(val_loader, model, criterion, normalizer, test=False):
 
         # compute output
         output = model(*input_var)
-        loss = criterion(output, target_var)
-
+        loss = lossfn(output, target_var, col_ign, criterion, "validation", epoch)
+        print("outshape", target.shape)
         # measure accuracy and record loss
-        if args.task == 'regression':
+        if args.task and {'regression', 'r'}:
             mae_error = mae(normalizer.denorm(output.data.cpu()), target)
             losses.update(loss.data.cpu().item(), target.size(0))
             mae_errors.update(mae_error, target.size(0))
             if test:
                 test_pred = normalizer.denorm(output.data.cpu())
                 test_target = target
-                test_preds += test_pred.view(-1).tolist()
-                test_targets += test_target.view(-1).tolist()
+                test_preds = torch.cat((test_preds, test_pred), 0)
+                test_targets = torch.cat((test_targets, test_target), 0)
                 test_cif_ids += batch_cif_ids
         else:
             accuracy, precision, recall, fscore, auc_score = \
@@ -388,8 +421,8 @@ def validate(val_loader, model, criterion, normalizer, test=False):
                 test_pred = torch.exp(output.data.cpu())
                 test_target = target
                 assert test_pred.shape[1] == 2
-                test_preds += test_pred[:, 1].tolist()
-                test_targets += test_target.view(-1).tolist()
+                test_preds = torch.cat((test_preds, test_pred), 0)
+                test_targets = torch.cat((test_targets, test_target), 0)
                 test_cif_ids += batch_cif_ids
 
         # measure elapsed time
@@ -397,7 +430,7 @@ def validate(val_loader, model, criterion, normalizer, test=False):
         end = time.time()
 
         if i % args.print_freq == 0:
-            if args.task == 'regression':
+            if args.task and {'regression', 'r'}:
                 print('Test: [{0}/{1}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
@@ -419,15 +452,20 @@ def validate(val_loader, model, criterion, normalizer, test=False):
 
     if test:
         star_label = '**'
-        import csv
+        print("preds:", test_preds.shape)
+        print("targs:", test_targets.shape)
+        print("cifs:", len(test_cif_ids))
+        test_outs = torch.cat((test_targets, test_preds), 1)
+        # import csv
         with open('test_results.csv', 'w') as f:
-            writer = csv.writer(f)
-            for cif_id, target, pred in zip(test_cif_ids, test_targets,
-                                            test_preds):
-                writer.writerow((cif_id, target, pred))
+            for i in range(len(test_cif_ids)):
+                f.write(", ".join(map(str, [test_cif_ids[i], *test_outs[i, :].detach().tolist()])) + "\n")
+            # writer = csv.writer(f)
+        # for i in zip(test_cif_ids, *test_targets, *test_preds):
+        #   writer.writerow(i)
     else:
         star_label = '*'
-    if args.task == 'regression':
+    if args.task and {'regression', 'r'}:
         print(' {star} MAE {mae_errors.avg:.3f}'.format(star=star_label,
                                                         mae_errors=mae_errors))
         return mae_errors.avg
